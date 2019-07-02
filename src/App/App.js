1c405acdc17422f94f1677ab6204b54fb59194a2
DeMarcus Kirby <tkd024@gmail.com>
2019-07-02 15:45:31 -0600

    Added fetch calls
    Co-Authored-By: Steve Rumizen <steverumizen@gmail.com>

--- src/App/App.js
+++ src/App/App.js
@@ -13,8 +13,49 @@ class App extends Component {
       planets: [],
       vehicles: [],
       faveCount: 0,
-      movie: 'Sample'
+      movie: '',
+      error: ''
     };
+  }
+
+  componentDidMount() {
+    this.getPeople();
+    this.getVehicles();
+    this.getPlanets();
+    this.getFilms();
+  }
+
+  getPeople = () => {
+    fetch('https://swapi.co/api/people/?page=1')
+      .then(response => response.json())
+      .then(data => this.setState({ people: data.results }))
+      .catch(error => this.setState({ error: error.message }))
+  }
+
+  getPlanets = () => {
+    fetch('https://swapi.co/api/planets/?page=1')
+      .then(response => response.json())
+      .then(data => this.setState({ planets: data.results }))
+      .catch(error => this.setState({ error: error.message }))
+  }
+
+  getVehicles = () => {
+    fetch('https://swapi.co/api/vehicles/?page=1')
+      .then(response => response.json())
+      .then(data => this.setState({ vehicles: data.results }))
+      .catch(error => this.setState({ error: error.message }))
+  }
+
+  getFilms = () => {
+    fetch('https://swapi.co/api/films/?page=1')
+      .then(response => response.json())
+      .then(data => this.setState({ movie: this.chooseMovie(data.results)}))
+      .catch(error => this.setState({ error: error.message }))
+  }
+
+  chooseMovie = data => {
+    const randomIndex = Math.floor(Math.random() * Math.floor(10))
+    return data[randomIndex].opening_crawl
   }
 
   render() {
