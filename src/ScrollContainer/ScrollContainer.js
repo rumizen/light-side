f601aefcedfb5249ef175d34deae9ba58fda912f
Steve Rumizen <steverumizen@gmail.com>
2019-07-08 15:34:30 -0600

    Add scroll text component
    Co-Authored-By: De'Marcus Kirby <tkd024@gmail.com>

--- src/ScrollContainer/ScrollContainer.js
+++ src/ScrollContainer/ScrollContainer.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import "react-star-wars-crawl/lib/index.css";
 import './ScrollContainer.scss';
+import Crawl from "react-star-wars-crawl";
 
 const ScrollContainer = ({ movie }) => {
   return (
-    <section className="scroll-container">
-      {movie}
-    </section>
-  )
+    <Crawl
+      className="scroll-container"
+      title={movie.episode_id}
+      subTitle={movie.title}
+      text={movie.opening_crawl}
+      height="20%"
+    />
+  );
 }
 
 export default ScrollContainer;
